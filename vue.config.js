f762b40df70729d62b7418657c70cfeb14c50a0a
Renaud ASTE <renaud.aste@ocea-sb.com>
2022-08-25 10:58:44 +0200

    init repo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  // publicPath: '', // <-- has to set this even if build target is lib.
   transpileDependencies: true
 })
